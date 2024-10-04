5c6eb2514f4e898aeb9979316aab2165278ca765
Rozengarden <github@rozengarden.eu>
2024-10-04 23:01:01 +0200

    fix: correct test

--- tests/test_WonderTrade.py
+++ tests/test_WonderTrade.py
@@ -22,12 +22,12 @@ def trade(project, alice, YBC):
 def macci(YBC, alice):
     a = accounts["0xA8a849b09AA9bf62782AB572f09FF479B76EBaC8"]
     a.balance = 1000000000000000000000
-    YBC.mintbaseExisting([alice], [TOKEN_ID1], [1], sender=a)
-    YBC.mintbaseExisting([a], [TOKEN_ID2], [1], sender=a)
+    YBC.mintBaseExisting([alice], [TOKEN_ID1], [1], sender=a)
+    YBC.mintBaseExisting([a], [TOKEN_ID2], [1], sender=a)
     return a
 
 @pytest.fixture
-def test_deposit(trade, alice, macci, YBC):
+def test_seed(trade, alice, macci, YBC):
     before = YBC.balanceOf(alice, TOKEN_ID1)
     YBC.safeTransferFrom(alice, trade, TOKEN_ID1, 1, 0, sender=alice)
     assert YBC.balanceOf(alice, TOKEN_ID1) - before == -1
@@ -35,11 +35,11 @@ def test_deposit(trade, alice, macci, YBC):
     assert trade.seeder() == alice
 
 @pytest.fixture
-def test_main(test_deposit, trade, alice, macci, YBC):
-    macci_before_1 = YBC.balance(macci, TOKEN_ID1)
-    macci_before_2 = YBC.balance(macci, TOKEN_ID2)
-    assert YBC.balance(trade.address, TOKEN_ID1) == 1
-    assert YBC.balance(trade.address, TOKEN_ID2) == 0
+def test_main(test_seed, trade, alice, macci, YBC):
+    macci_before_1 = YBC.balanceOf(macci, TOKEN_ID1)
+    macci_before_2 = YBC.balanceOf(macci, TOKEN_ID2)
+    assert YBC.balanceOf(trade.address, TOKEN_ID1) == 1
+    assert YBC.balanceOf(trade.address, TOKEN_ID2) == 0
     YBC.safeTransferFrom(macci, trade, TOKEN_ID2, 1, 0, sender=macci)
     assert YBC.balanceOf(macci, TOKEN_ID1) - macci_before_1 == 1
     assert YBC.balanceOf(macci, TOKEN_ID2) - macci_before_2 == -1
@@ -48,15 +48,23 @@ def test_main(test_deposit, trade, alice, macci, YBC):
     assert trade.isSeeded()
     assert trade.seeder() == alice
 
-def test_withdraw(test_deposit, trade, YBC, alice):
+def test_withdraw(test_seed, trade, YBC, alice):
     before = YBC.balanceOf(alice, TOKEN_ID1)
     trade.withdraw(sender=alice)
     assert YBC.balanceOf(alice, TOKEN_ID1) - before == 1
-    assert !trade.isSeeded()
+    assert not trade.isSeeded()
     assert trade.seeder() == alice
+
+def test_withdraw_after_exchange(test_main, trade, YBC, alice):
+    before = YBC.balanceOf(alice, TOKEN_ID2)
+    trade.withdraw(sender=alice)
+    assert YBC.balanceOf(alice, TOKEN_ID2) - before == 1
+    assert not trade.isSeeded()
+    assert trade.seeder() == alice
+
 
 # TODO:
 # different erc1155 collection
-# test withdraw without main & unauthorized withdraw
+# test withdraw with main & unauthorized withdraw
 # test sending multiple token and batch
 # test rescue function
